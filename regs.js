cd6248cd9a0a66d7c37be4203dde1cb563ae425d
Noran <281170419@qq.com>
2018-11-04 16:02:10 +0800

    add number reg

--- regs.js
+++ regs.js
@@ -2,4 +2,22 @@ var positiveNumber = /^\d+(\.\d+|\d*)$/i;
 var notEmptyString = /^(?!\s*$).+/i;
 var emptyString = /^\s*$/i;
 
+onlyNumber(){
+	onKeyup(evt) {
+    const reg = /[^\.\d]/g;
+    const dotReg = /\./g;
+    const key = evt.key;
+    const value = evt.target.value;
+
+    if (reg.test(key)) {
+      evt.target.value = value.replace(key, '');
+    } else {
+      if (key === '.' && value.match(dotReg).length > 1) {
+        const dotIndex = value.lastIndexOf('.');
+        evt.target.value = value.substring(0, dotIndex) + value.substring(dotIndex + 1);
+      }
+    }
+  }
+}
+
 
